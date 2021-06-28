d4ac126cb3ca69ee7a1a954ff32d51d9f6ab4894
beebrsk <burutdakorn_k@cmu.ac.th>
2021-06-29 01:37:36 +0700

    showlist

--- Newjs.js
+++ Newjs.js
@@ -1,33 +1,35 @@
-function onLoad(){
-    document.getElementById('searchButton').addEventListener('click',searchAnime)
+var show_content = document.getElementById('outputmylist');
+console.log(show_content.innerHTML);
+
+function onLoad() {
+    document.getElementById('searchButton').addEventListener('click', searchAnime)
 }
 //----------     เริ่มค้นหาจากapi  ----------------------//
-function searchAnime(event)
-{
+function searchAnime(event) {
     opensearch()
     let query = document.getElementById('searchInput').value
     fetch(`https://api.jikan.moe/v3/search/anime?q=${query}`,)
-    .then ((response) =>{
-        return response.json()
-    }).then((data) => {
-        data.results.map((element) =>{
-            console.log(element)
-            addAnimeToCard(element)
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            data.results.map((element) => {
+                // console.log(element)
+                addAnimeToCard(element)
+            })
+        }).catch((err) => {
+            console.warn(err.message)
         })
-    }).catch((err) => {
-        console.warn(err.message)
-    })
 }
 //----------  หลังจากดึงข้อมูลเสร็จก็เอามาใส่ในการ์ด     ----------------------//
-function addAnimeToCard(anime){
-    const searchList= document.getElementById('searchList')
+function addAnimeToCard(anime) {
+    const searchList = document.getElementById('searchList')
     let card = document.createElement('div')
     card.classList.add("searchcard")
     let img = document.createElement('img')
-    img.style.height="300px"
-    img.setAttribute('src',anime.image_url)
-    img.addEventListener('dblclick',function(){
-        addToFavAnime()
+    img.style.height = "300px"
+    img.setAttribute('src', anime.image_url)
+    img.addEventListener('dblclick', function () {
+        addToFavAnime(anime)
     })
     card.appendChild(img)
     let titleAnime = document.createElement('h5')
@@ -35,50 +37,90 @@ function addAnimeToCard(anime){
 
     card.appendChild(titleAnime)
     searchList.appendChild(card)
-    
-}
 
+}
+//---------------------เเสดงlist------------------------------------//
+function showcontentAdd(list) {
+    output2.innerHTML += `
+        <div class="">
+            <div class="">
+                <img src="${list.image_url}">
+                <h1>${list.title}</h1>
+            </div>
+        </div>
+     `
+}
 //---------- ใช้ตกลง      ----------------------//
-function addToFavAnime(){
+function addToFavAnime(anime) {
     let answer = confirm('Are you sure')
     console.log(answer)
-    if(answer==true){
+    if (answer == true) {
         console.log('gogo')
-        addSearchToDB(anime)
         closesearch()
+        addAnimeToData(anime)
     }
-                     
+
 }
 //----------   ปิดหน้าข้อมูลค้นหา    ----------------------//
-function closesearch(){
-    document.getElementById('searchList').style.display='none'      
+function closesearch() {
+    document.getElementById('searchList').style.display = 'none'
 }
-function opensearch(){
-    document.getElementById('searchList').style.display='flex'
-    
-        
+function opensearch() {
+    document.getElementById('searchList').style.display = 'flex'
+
+
 }
-document.getElementById('myListnav').addEventListener('click',function(){
-    function showMyList(){
-        outputmylist.innerHTML = ''
-        fetch(`https://se104-project-backend.du.r.appspot.com/movie/632110344`)
-        .then((response) =>{
-            return response.json()
-        }).then(data =>{
-            mylist(data)
-        })
-    }
+document.getElementById('myListnav').addEventListener('click', function () {
+    closesearch()
 })
 
+function showMyList() {
+    fetch('https://se104-project-backend.du.r.appspot.com/movies/632110344', {
+        method: 'get'
+    })
+        .then((response) => {
+            return response.json()
+        }).then(data => {
+            data.map((val) => {
+                showcontentAdd(val)
+            });
+        })
+}
+
+function getdetail(id) {
+    fetch(`https://se104-project-backend.du.r.appspot.com/movie/632110342/${id}`)
+        .then((response) => {
+            return response.json()
+        }).then(data => {
+            const detail = document.getElementById('Detail')
+            detail.innerHTML = ''
+            showDet()
+            showDetail(data);
+        })
+}
 
 //----------  เพิ่มข้อมูลเข้าdata     ----------------------//
 function addAnimeToData(anime) {
-    
+    const data = {
+        "id": "632110344",
+        "movie": {
+            "url": anime.url,
+            "image_url": anime.image_url,
+            "title": anime.title,
+            "synopsis": anime.synopsis,
+            "type": anime.type,
+            "episodes": anime.episodes,
+            "score": anime.score,
+            "rated": anime.reted
+        }
+    }
+
     fetch(`https://se104-project-backend.du.r.appspot.com/movies`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
+        body: JSON.stringify(data)
     }).then((response) => {
         if (response.status === 200) {
             return response.json()
@@ -86,12 +128,7 @@ function addAnimeToData(anime) {
             throw Error(response.statusText)
         }
     }).then(data => {
-
-        console.log(data)
-        output.innerHTML=''
-        showMyList()
-        
-        
+        showMyList();
     })
 }
 
